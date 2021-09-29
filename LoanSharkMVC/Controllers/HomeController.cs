8743417da557f75c760376b788122b0c95b217ea
arunpun <83875939+arunpun@users.noreply.github.com>
2021-09-29 19:50:27 +0100

    Add project files.

---
+++ LoanSharkMVC/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using LoanSharkMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoanSharkMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult App()
+        {
+            Loan loan = new();
+
+            loan.Amount = 15000m;
+            loan.Payment = 0.0m;
+            loan.Rate = 3.5m;
+            loan.Term = 60;
+            loan.TotalCost = 0.0m;
+            loan.TotalInterest = 0.0m;
+
+            return View(loan); //The default values will be displayed to the user
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LoanSharkMVC/Models/Loan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoanSharkMVC.Models
+{
+    public class Loan
+    {
+        public decimal Amount { get; set; }
+        public decimal Rate { get; set; }
+        public int Term { get; set; } //In months
+        public decimal Payment { get; set; } //Monthly payment
+        public decimal TotalInterest { get; set; } //Interest over life of the loan
+        public decimal TotalCost { get; set; } //Amount borrowed plus total interest
+
+        public List<LoanPayment> Payments = new List<LoanPayment>(); //Will hold instance of loan payments, for a 12 month loan, it will hold 12 instances of loan payment
+
+    }
+}
